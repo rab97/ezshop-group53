009586bb3c08d4f8be7228f406a36137fdae183a
Marco Sapio s291691 <s291691@studenti.polito.it>
2021-05-16 15:18:14 +0200

    add test for Costumer and BalanceOpeartion

--- src/test/java/it/polito/ezshop/test/OperatorTest.java
+++ src/test/java/it/polito/ezshop/test/OperatorTest.java
@@ -13,92 +13,92 @@ public class OperatorTest {
 	Operator o = new Operator();
 	
 	@Test
-	public void TestIsValid1() {
+	public void testIsValid1() {
 		String s = "123456789104";
 		assertTrue(o.isValidCode(s));
 	}
 	
 	@Test
-	public void TestIsValid2() {
+	public void testIsValid2() {
 		String s = "4563789345138";
 		assertTrue(o.isValidCode(s));
 	}
 	
 	@Test
-	public void TestIsValid3() {
+	public void testIsValid3() {
 		String s = "45637485902647";
 		assertTrue(o.isValidCode(s));
 	}
 	
 	
 	@Test
-	public void TestIsValid4() {
+	public void testIsValid4() {
 		String s = "12345678910";
 		assertFalse(o.isValidCode(s));
 	}
 	
 	@Test
-	public void TestIsValid5() {
+	public void testIsValid5() {
 		String s = "456374859026475";
 		assertFalse(o.isValidCode(s));
 	}
 	
 	@Test
-	public void TestIsValid6() {
+	public void testIsValid6() {
 		String s = "";
 		assertFalse(o.isValidCode(s));
 	}
 	
 	@Test
-	public void TestIsValid7() {
+	public void testIsValid7() {
 		String s = null;
 		assertFalse(o.isValidCode(s));
 	}
 	
 	@Test
-	public void TestLuhnCheck1() {
+	public void testLuhnCheck1() {
 		String s = "4485370086510891";
 		assertTrue(o.luhnCheck(s));
 	}
 	
 	@Test
-	public void TestLuhnCheck2() {
+	public void testLuhnCheck2() {
 		String s = "4716258050958645";
 		assertTrue(o.luhnCheck(s));
 	}
 	
 	@Test
-	public void TestLuhnCheck3() {
+	public void testLuhnCheck3() {
 		String s = "45637485902647";
 		assertFalse(o.luhnCheck(s));
 	}
 	
 	@Test
-	public void TestLuhnCheck4() {
+	public void testLuhnCheck4() {
 		String s = "123456789102345";
 		assertFalse(o.luhnCheck(s));
 	}
 	
 	@Test
-	public void TestLuhnCheck5() {
+	public void testLuhnCheck5() {
 		String s = "1234567812345678";
 		assertFalse(o.luhnCheck(s));
 	}
 	
 	@Test
-	public void TestLuhnCheck6() {
+	public void testLuhnCheck6() {
 		String s = "";
 		assertFalse(o.luhnCheck(s));
 	}
 	
 	@Test
-	public void TestLuhnCheck7() {
+	public void testLuhnCheck7() {
 		String s = null;
 		assertFalse(o.luhnCheck(s));
 	}
 	
 	@Test
-	public void TestLuhnCheck8() {
+	public void testLuhnCheck8() {
 		String s = "ashudsallidò";
 		assertFalse(o.luhnCheck(s));
 	}
